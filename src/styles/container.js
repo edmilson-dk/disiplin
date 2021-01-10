3b420cf7fc77c3dd9f5c0bf5fbbf26dc79e1e409
DKSecurity99 <dksecurity@gmail.com>
2021-01-10 09:30:30 -0300

    feat: added media queries in container

--- src/styles/container.js
+++ src/styles/container.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 0;
+  width: 100%;
   max-width: 39rem;
   padding: 0 1rem;
   margin: 0 auto;
+
+  @media (min-width: 740px) {
+    max-width: 72rem;
+  }
+  
+  @media (min-width: 980px) {
+    max-width: 94rem;
+  }
 `;
 
 export default Container;
