edb99037f505b3ab886b9e72be4cfc957cae1fc1
DKSecurity99 <dksecurity@gmail.com>
2021-01-10 09:29:51 -0300

    imported theme and global styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import GlobalStyles from '../styles/globals';
+import Theme from '../styles/theme';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Theme>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </Theme>
+  )
 }
 
 export default MyApp;
